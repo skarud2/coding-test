0e3e8e6993255f016e3ec74023dd8a25f57bd6c0
nk <121710775+skarud2@users.noreply.github.com>
2025-06-01 00:00:48 +0900

    [Gold V] Title: 토마토, Time: 384 ms, Memory: 229456 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,57 @@
+#2025-05-30
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+x, y=map(int, input().rsplit())
+box=[]
+
+for _ in range(y):
+    box.append(list(map(int, input().rsplit())))
+
+one=deque([])
+empty=0
+zero=0
+for i in range(y):
+    for j in range(x):
+        if box[i][j]==1:
+            one.append((i,j,0))
+        if box[i][j]==-1:
+            empty+=1
+        if box[i][j]==-1:
+            zero+=1
+
+if zero==x*y:
+    print(-1)
+    exit()
+
+if len(one)==x*y:   #1이 하나도 없을 때
+    print(0)
+    exit()
+
+if empty==x*y:   #다 -1일 때
+    print(-1)
+    exit()
+
+while one:
+    a,b,d=one.popleft()
+    if a>0 and box[a-1][b]==0:   #상
+        box[a-1][b]=1
+        one.append((a-1,b,d+1))
+    if a<y-1 and box[a+1][b]==0:   #하
+        box[a+1][b]=1
+        one.append((a+1,b,d+1))
+    if b>0 and box[a][b-1]==0:  #좌
+        box[a][b-1]=1
+        one.append((a,b-1,d+1))
+    if b<x-1 and box[a][b+1]==0:   #우
+        box[a][b+1]=1
+        one.append((a,b+1,d+1))
+        
+for i in range(y):
+    for j in range(x):
+        if box[i][j]==0:   #0이 남아있을 경우
+            print(-1)
+            exit()
+    
+print(d)
